f2de3dc35678cb144f9042e0a1a194ec037317c1
willy161 <gasper.jezovsek@gmail.com>
2025-11-20 18:47:51 +0100

    Maven structure

---
+++ src/main/java/com/golfapp/igrisce/IgrisceApplication.java
@@ -0,0 +1,13 @@
+package com.golfapp.igrisce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrisceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrisceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golfapp/igrisce/IgrisceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golfapp.igrisce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgrisceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
